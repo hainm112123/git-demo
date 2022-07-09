09be56cde34fa0b36e90bc00e9f7ff3c5fbcaf72
yuuhi <hainm112123@gmail.com>
2022-07-09 21:32:03 +0700

    add chicken function

--- chicken.js
+++ chicken.js
@@ -1,6 +1,11 @@
-var chicken = function(name , weight) {
+var Chicken = function(name , weight) {
   this.name = name;
   this.weight = weight;
+  this.alive = true;
+}
+
+Chicken.prototype.die = function() {
+  this.alive = false;
 }
 
 module.exports;
